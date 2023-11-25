b69aba78369c4833a4000ef2b060c527ddc55908
Ben-David <samuelbendavid01@gmail.com>
2023-11-26 00:46:59 +0100

    working on blog nav

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -16,7 +16,7 @@ def show_latest_posts(count=5):
     return {'latest_posts': latest_posts}
 
 @register.simple_tag
-def get_most_commented_posts(count=5):
+def get_most_commented_posts(count=3):
     return Post.published.annotate(
         total_comments=Count('comments')
         ).order_by('-total_comments')[:count]
