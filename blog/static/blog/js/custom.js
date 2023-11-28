d18ee79b7cfa48ad51fac814248e1819eec7e58f
Ben-David <samuelbendavid01@gmail.com>
2023-11-28 23:51:50 +0100

    working on nav

--- blog/static/blog/js/custom.js
+++ blog/static/blog/js/custom.js
@@ -16,16 +16,17 @@ jQuery( document ).ready(function( $ ) {
         
 
         $(window).scroll(function() {
-          var scroll = $(window).scrollTop();
-          var box = $('.header-text').height();
-          var header = $('header').height();
-
-          if (scroll >= box - header) {
-            $("header").addClass("background-header");
-          } else {
-            $("header").removeClass("background-header");
-          }
+            var scroll = $(window).scrollTop();
+            var box = $('.header-text').height();
+            var header = $('header').height();
+        
+            if (scroll >= box - header) {
+                $("header").addClass("background-header");
+            } else {
+                $("header").removeClass("background-header");
+            }
         });
+        
 
         if ($('.owl-clients').length) {
             $('.owl-clients').owlCarousel({
