1b73181bc23cd13c82ff5cba6343c4550abf6868
Ben-David <samuelbendavid01@gmail.com>
2023-11-29 04:15:09 +0100

    added image to the blog

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
 
-# Create your models here.
-
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
@@ -20,6 +18,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)  # Image field
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -28,15 +27,15 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT,
     )
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
     tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
-            ]
+        ]
 
     def __str__(self):
         return self.title
@@ -45,9 +44,9 @@ class Post(models.Model):
         return reverse(
             'blog:post_detail', 
             args=[self.publish.year,
-            self.publish.month,
-            self.publish.day,
-            self.slug])
+                  self.publish.month,
+                  self.publish.day,
+                  self.slug])
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -70,4 +69,3 @@ class Comment(models.Model):
         
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
-    
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
         name='django.contrib.sitemaps.views.sitemap')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
